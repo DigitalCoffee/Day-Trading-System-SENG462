5739f0f1c974bb4b23dd5c58ef3b833222522d97
dbaylyuvic <dbayly@uvic.ca>
2016-03-15 16:46:06 -0600

    Added files via upload

---
+++ Interface/Database.java
@@ -0,0 +1,17 @@
+package Interface;
+import java.rmi.*;
+import java.sql.*;
+import Transaction.Quote;
+
+
+//import Transaction.TransactionObjects;
+public interface Database extends Remote{
+public static final String LOOKUPNAME = "rmi://b140.seng.uvic.ca:44459/dbserver";
+public ResultSet get(String cmd);
+public boolean set(String cmd);
+public void checkTriggers(String stk,long timestmp,double amount,String cryptokey);
+//public boolean add(String cmd);
+public boolean buy(String uid, String stk,double amount,Quote q);
+public boolean sell(String uid,String stk, double amount,Quote q);
+public String buycom(String userid);
+}
