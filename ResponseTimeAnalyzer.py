2712f47b6effa44e761658812f2900a7f829c51e
DigitalCoffee <a.hansen00@gmail.com>
2016-04-04 22:48:12 -0700

    NGINX config files

--- ResponseTimeAnalyzer.py
+++ ResponseTimeAnalyzer.py
@@ -3,7 +3,26 @@
 import sys
 import re
 
-output_regex = r"(COMMAND TOOK TOO LONG!!!!!!)?.*:\s*(\d+\.\d*)\s*msecs"
+output_regex = r"(COMMAND TOOK TOO LONG!!!!!!)?.*\[\d+\]\s*(\w+).*:\s*(\d+\.\d*)\s*msecs"
+
+commands = {
+        "ADD": [],
+        "QUOTE": [],
+        "BUY": [],
+        "SELL": [],
+        "COMMIT_BUY": [],
+        "COMMIT_SELL": [],
+        "CANCEL_BUY": [],
+        "CANCEL_SELL": [],
+        "SET_BUY_AMOUNT": [],
+        "SET_SELL_AMOUNT": [],
+        "SET_BUY_TRIGGER": [],
+        "SET_SELL_TRIGGER":[],
+        "CANCEL_SET_BUY": [],
+        "CANCEL_SET_SELL": [],
+        "DISPLAY_SUMMARY": [],
+        "DUMPLOG": []
+        }
 
 def main(argv):
     total_msecs = 0
@@ -15,15 +34,19 @@ def main(argv):
     fo = open(argv[1], "r+")
     for line in fo:
         m = re.match(output_regex, line.strip())
-        if m and m.group(2):
+        if m and m.group(3):
             if m.group(1):
                 slow_commands = slow_commands + 1
-            total_msecs = total_msecs + float(m.group(2))
+            commands[m.group(2)].append(float(m.group(3)))
+            total_msecs = total_msecs + float(m.group(3))
             total_commands = total_commands + 1
 
-    print "Average response time for " + str(total_commands) + ":"
-    print str(total_msecs / total_commands)
+    print "Average response time for " + str(total_commands) + ": " + str(total_msecs / total_commands) + "msecs"
     print "Late commands: " + str(slow_commands)
+
+    for k,v in commands.iteritems():
+        if len(v) > 0:
+            print k + " Average: " + str(sum(v)/len(v)) + ", Min: " + str(min(v)) + ", Max: " + str(max(v))
 
 
 if __name__ == "__main__":
